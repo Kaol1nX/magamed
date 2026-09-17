7a7f85b07518a42110c517f1bba2f6d689c59d0d
Kaol1nX <zary1307@mail.ru>
2026-09-17 17:48:38 +0500

    парапара

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/you', (req, res) => {
+  res.send('Ты че мычишь вась');
+});
+
+app.get('/dota', (req, res) => {
+  res.send('3000mmr');
+});
+     
+app.get('/bestcarry', (req, res) => {
+  res.send('YatoroGOD');
+});
+
+app.get('/Bestiq', (req, res) => {
+  res.send('staryibog');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
